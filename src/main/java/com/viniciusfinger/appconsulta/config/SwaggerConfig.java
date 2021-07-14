8ade29078f1c49ed72317e0aa8b0e0d2f12bf8ea
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2021-07-14 13:18:23 -0300

    Implementação do Swagger-ui

---
+++ src/main/java/com/viniciusfinger/appconsulta/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.viniciusfinger.appconsulta.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.viniciusfinger.appconsulta"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfo(
+                "Consultation API",
+                "REST API for consultation schedul.e",
+                "1.0",
+                "Terms of Service",
+                new Contact("Vinicius finger", "https://www.github.com/viniciusfinger",
+                        "fingervinicius@icloud.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+    }
+}
--- src/main/java/com/viniciusfinger/appconsulta/resource/HealthcareProfessionalResource.java
+++ src/main/java/com/viniciusfinger/appconsulta/resource/HealthcareProfessionalResource.java
@@ -3,6 +3,8 @@ package com.viniciusfinger.appconsulta.resource;
 import com.viniciusfinger.appconsulta.model.HealthcareProfessional;
 import com.viniciusfinger.appconsulta.model.dto.HealthcareProfessionalDTO;
 import com.viniciusfinger.appconsulta.service.HealthcareProfessionalService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,37 +13,45 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/healthcareProfessionals")
+@Api(value = "Healthcare Professionals")
+@CrossOrigin(origins = "*")
 public class HealthcareProfessionalResource {
 
     @Autowired
     private HealthcareProfessionalService service;
 
     @GetMapping
+    @ApiOperation(value = "Returns a list of Healthcare Professionals")
     public ResponseEntity<List<HealthcareProfessional>> findAll() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Return Healthcare Professional by Id")
     public ResponseEntity<HealthcareProfessional> findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
     @PostMapping
+    @ApiOperation(value = "Insert a new HealthcareProfessional")
     public ResponseEntity<HealthcareProfessional> save(@RequestBody HealthcareProfessionalDTO healthcareProfessionalDTO) {
         return service.save(healthcareProfessionalDTO);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete a Healthcare Professional")
     public ResponseEntity<?> delete(@PathVariable Long id){
         return service.delete(id);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update Healthcare Professional")
     public ResponseEntity<HealthcareProfessional> update(@RequestBody HealthcareProfessionalDTO updatedHealthcareProfessionalDTO, @PathVariable  Long id){
         return service.update(updatedHealthcareProfessionalDTO, id);
     }
 
     @GetMapping("/byPatient/{patientId}")
+    @ApiOperation(value = "Return Healthcare Professionals by Patient")
     public ResponseEntity<List<HealthcareProfessional>> findHealthcareProfessionalByPatient(@PathVariable Long patientId){
         return service.findHealthcareProfessionalByPatient(patientId);
     }
