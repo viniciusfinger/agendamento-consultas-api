bf978f9ac68b89b3e02b16c139c5a8a53b38ed4f
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2021-07-14 13:28:02 -0300

    Adição de anotações do Swagger

--- src/main/java/com/viniciusfinger/appconsulta/resource/MedicalConsultationResource.java
+++ src/main/java/com/viniciusfinger/appconsulta/resource/MedicalConsultationResource.java
@@ -5,6 +5,7 @@ import com.viniciusfinger.appconsulta.model.MedicalConsultation;
 import com.viniciusfinger.appconsulta.model.Patient;
 import com.viniciusfinger.appconsulta.model.dto.MedicalConsultationDTO;
 import com.viniciusfinger.appconsulta.service.MedicalConsultationService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,26 +21,31 @@ public class MedicalConsultationResource {
     private MedicalConsultationService service;
 
     @GetMapping
+    @ApiOperation(value = "Return a list of Medical Consultation")
     public ResponseEntity<List<MedicalConsultation>> findAll(){
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Return a Medical Consultation by Id")
     public ResponseEntity<MedicalConsultation> findById(@PathVariable Long id){
         return service.findById(id);
     }
 
     @PostMapping
+    @ApiOperation(value = "Schedule a new Medical Consultation")
     public ResponseEntity<MedicalConsultation> scheduleMedicalConsultation(@RequestBody MedicalConsultationDTO medicalConsultationDTO) throws Exception {
         return service.scheduleMedicalConsultation(medicalConsultationDTO);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Unschedule a Medical Consultation by Id")
     public ResponseEntity<?> unscheduleMedicalConsultation(@PathVariable Long id){
         return service.unscheduleMedicalConsultation(id);
     }
 
     @PostMapping("/finish/{id}")
+    @ApiOperation(value = "Finish a Medical Consultation by Id")
     public ResponseEntity<MedicalConsultation> finish(@PathVariable Long id){
         return service.finish(id);
     }
--- src/main/java/com/viniciusfinger/appconsulta/resource/PatientResource.java
+++ src/main/java/com/viniciusfinger/appconsulta/resource/PatientResource.java
@@ -3,6 +3,7 @@ package com.viniciusfinger.appconsulta.resource;
 import com.viniciusfinger.appconsulta.model.Patient;
 import com.viniciusfinger.appconsulta.model.dto.PatientDTO;
 import com.viniciusfinger.appconsulta.service.PatientService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,51 +18,61 @@ public class PatientResource {
     private PatientService service;
 
     @GetMapping
+    @ApiOperation(value = "Return a list of Patients")
     public ResponseEntity<List<Patient>> findAll() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Return a Patient by Id")
     public ResponseEntity<Patient> findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
     @GetMapping("/online")
+    @ApiOperation(value = "Return a list of all online Patients")
     public ResponseEntity<List<Patient>> findAllOnline(){
         return service.findAllOnline();
     }
 
     @GetMapping("/busy")
+    @ApiOperation(value = "Return a list of all busy Patients")
     public ResponseEntity<List<Patient>> FindAllBusy(){
         return service.findAllBusy();
     }
 
     @GetMapping("/offline")
+    @ApiOperation(value = "Return a list of all offline Patients")
     public ResponseEntity<List<Patient>> FindAllOffline(){
         return service.findAllOffline();
     }
 
     @GetMapping("/username/{username}")
+    @ApiOperation(value = "Return a Patient by username")
     public ResponseEntity<Patient> findByUsername(@PathVariable String username){
         return service.findByUsername(username);
     }
 
     @GetMapping("/crmProfessional/{crmProfessional}")
+    @ApiOperation(value = "Return a Patient by crm of Healthcare Professional")
     public ResponseEntity<List<Patient>> findByCrmProfessional(@PathVariable String crmProfessional){
         return service.findByCrmProfessional(crmProfessional);
     }
 
     @PostMapping
+    @ApiOperation(value = "Save a new Patient")
     public ResponseEntity<Patient> save(@RequestBody PatientDTO patientDTO) {
         return service.save(patientDTO);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update a Patient by Id")
     public ResponseEntity<Patient> update(@RequestBody PatientDTO updatedPatientDTO, @PathVariable  Long id){
         return service.update(updatedPatientDTO, id);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete a Patient by Id")
     public ResponseEntity<?> delete(@PathVariable Long id){
         return service.delete(id);
     }
