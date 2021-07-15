60d6e1488c1f0df1cdf9ce9d0fb911d116f57c9e
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2021-07-14 21:26:28 -0300

    Implementação JWT

---
+++ src/main/java/com/viniciusfinger/appconsulta/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.viniciusfinger.appconsulta.config;
+
+import com.viniciusfinger.appconsulta.model.JwtAuthenticationFilter;
+import com.viniciusfinger.appconsulta.model.JwtLoginFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("ADMIN");
+    }
+}
---
+++ src/main/java/com/viniciusfinger/appconsulta/model/JwtLoginFilter.java
@@ -0,0 +1,50 @@
+package com.viniciusfinger.appconsulta.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.viniciusfinger.appconsulta.service.TokenAuthenticationService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    public JwtLoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+
+        Patient patient = new ObjectMapper() //aqui vai ler um objeto patient e transformar em Patient
+                .readValue(request.getInputStream(), Patient.class);
+
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        patient.getUsername(),
+                        patient.getPassword(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain,
+            Authentication auth) throws IOException, ServletException {
+        TokenAuthenticationService.addAuthentication(response, auth.getName());
+    }
+}
