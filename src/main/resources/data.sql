2dd349d05a22044b44abe6841cf7b3bcdec73847
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2021-07-15 20:07:10 -0300

    Alterações em dados para comportar o acesso do security ao banco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
+
 insert into status (name) values ('Online'),('Busy'),('Offine');
+
+--User: admin  Password: 12345
+insert into patient (username, password, enabled, status_id) values ('admin', '$2a$10$pmPLtjIzArn7UN3/WTZStOuV8Nk1dIFgyZ8MDVWf1ob4kGTPVwKGy', true, 1);
+insert into authorities values ('admin','ADMIN');
